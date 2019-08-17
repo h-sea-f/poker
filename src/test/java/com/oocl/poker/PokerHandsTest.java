6e6289262d2d9a49986184d5bd8057c918310447
h-sea-f <h_sea_f@163.com>
2019-08-17 16:44:57 +0800

    finish should_return_It_ends_in_a_draw_when_given_2H_2H

--- src/test/java/com/oocl/poker/PokerHandsTest.java
+++ src/test/java/com/oocl/poker/PokerHandsTest.java
@@ -16,4 +16,12 @@ public class PokerHandsTest {
         Assert.assertEquals(result,"play2 win");
     }
 
+    @Test
+    public void should_return_It_ends_in_a_draw_when_given_2H_2H(){
+        PokerHands pokerHands = new PokerHands();
+        String card1 = "2H";
+        String card2 = "2H";
+        String result = pokerHands.compare(card1,card2);
+        Assert.assertEquals(result,"It ends in a draw");
+    }
 }
