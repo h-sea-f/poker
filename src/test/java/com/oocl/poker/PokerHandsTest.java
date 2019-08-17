f925d88647fea22616ab07e4d23881ba04597bfd
h-sea-f <h_sea_f@163.com>
2019-08-17 21:43:09 +0800

    finish should_return_play1_win_when_given_3H_4D_5S_6C_7D_and_2H_3D_4S_5C_6D test and implement

--- src/test/java/com/oocl/poker/PokerHandsTest.java
+++ src/test/java/com/oocl/poker/PokerHandsTest.java
@@ -150,4 +150,13 @@ public class PokerHandsTest {
         String result = pokerHands.compare(card1, card2);
         Assert.assertEquals("It ends in a draw", result);
     }
+
+    @Test
+    public void should_return_play1_win_when_given_3H_4D_5S_6C_7D_and_2H_3D_4S_5C_6D() {
+        PokerHands pokerHands = new PokerHands();
+        String card1 = "3H 4D 5S 6C 7D";
+        String card2 = "2H 3D 4S 5C 6D";
+        String result = pokerHands.compare(card1, card2);
+        Assert.assertEquals("play1 win", result);
+    }
 }
