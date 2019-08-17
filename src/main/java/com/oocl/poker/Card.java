e58f2dbd120ffe2b28418044eddb316479d24300
h-sea-f <h_sea_f@163.com>
2019-08-17 16:41:59 +0800

    refactor should_return_play2_win_given_2H_3H test and implement

--- src/main/java/com/oocl/poker/Card.java
+++ src/main/java/com/oocl/poker/Card.java
@@ -1,14 +1,19 @@
 package com.oocl.poker;
 
-public class Card {
-    private String number;
-    private String shape;
-    private int position;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public Card(String number, String shape, int position) {
-        this.number = number;
-        this.shape = shape;
-        this.position = position;
+public class Card {
+    private char number;
+    private char shape;
+    private int position;
+    private List<Character> order = new ArrayList<>(Arrays.asList('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'));
+
+    public Card(String brand) {
+        this.number = brand.charAt(0);
+        this.shape = brand.charAt(1);
+        this.position = order.indexOf(brand.charAt(0));
     }
 
     public int getPosition() {
--- src/main/java/com/oocl/poker/PokerHands.java
+++ src/main/java/com/oocl/poker/PokerHands.java
@@ -3,11 +3,15 @@ package com.oocl.poker;
 import java.util.List;
 
 public class PokerHands {
-    public String compare(List<Card> play1, List<Card> play2) {
-        if(play1.get(0).getPosition()>play2.get(0).getPosition()) {
+    public String compare(String play1Card, String play2Card) {
+        Card card1 = new Card(play1Card);
+        Card card2 = new Card(play2Card);
+        if (card1.getPosition() > card2.getPosition()) {
             return "play1 win";
-        }else {
+        } else if (card1.getPosition() < card2.getPosition()) {
             return "play2 win";
+        } else {
+            return "It ends in a draw";
         }
     }
 }
